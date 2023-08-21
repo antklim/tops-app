ba78431fac8fa158b81c50d5a626858e028af75f
antklim <antklim@gmail.com>
2023-08-21 22:28:44 +1000

    chore: added path aliases

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, useColorScheme } from 'react-native'
 import { ThemeProvider } from "@react-navigation/native"
 
-import View from '../ui/components/view'
-import Text from '../ui/components/text'
-import DarkTheme from '../ui/themes/dark'
-import LightTheme from '../ui/themes/light'
+import View from '@/ui/components/view'
+import Text from '@/ui/components/text'
+import DarkTheme from '@/ui/themes/dark'
+import LightTheme from '@/ui/themes/light'
 
 const App = () => {
   const colorScheme = useColorScheme()
