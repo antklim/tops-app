3d49ce49d60671dd2dbdda6f9a198c7dc6a03524
antklim <antklim@gmail.com>
2023-09-01 08:53:45 +1000

    feat: added sign in submit to sign in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,35 @@
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet, TextInput } from 'react-native'
 import { useAuth } from 'context/auth'
 import { Text, View } from 'ui/component'
+import { useState } from 'react'
 
 const SignIn = () => {
   const { signIn } = useAuth()
 
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [signInEnabled, setSignInEnabled] = useState(true)
+
+  const submitSignIn = async () => {
+    setSignInEnabled(false)
+
+    try {
+      await signIn({ name, email })
+    } catch {
+    } finally {
+      setSignInEnabled(true)
+    }
+  }
+
+  // TODO: use text content type on iOS for inputs
+
   return (
     <View style={styles.container}>
-      <Text onPress={signIn}>Sign In</Text>
+      <TextInput style={styles.input} placeholder="Name" autoCapitalize="none" onChangeText={setName} />
+      <TextInput style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={setEmail} />
+      <Pressable onPress={submitSignIn} disabled={!signInEnabled}>
+        <Text>Sign In</Text>
+      </Pressable>
     </View>
   )
 }
@@ -19,5 +41,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 0.75,
+    borderRadius: 7,
+    margin: 8,
+    padding: 10,
+    width: 300,
   },
 })
