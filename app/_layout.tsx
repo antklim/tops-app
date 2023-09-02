6803fc5256a0c66b7d0d954a672c76c72ba9bdea
antklim <antklim@gmail.com>
2023-09-02 19:00:16 +1000

    feat: check if user is logged in

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@react-navigation/native'
-import { Slot } from 'expo-router'
-import React from 'react'
+import { Slot, SplashScreen } from 'expo-router'
+import React, { useEffect, useState } from 'react'
 import { useColorScheme } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
@@ -11,10 +11,28 @@ import { magic } from 'lib/auth/magic/client'
 // Catch any errors thrown by the Layout component.
 export { ErrorBoundary } from 'expo-router'
 
+SplashScreen.preventAutoHideAsync()
+
 const RootLayout = () => {
   const colorScheme = useColorScheme()
 
   const theme = colorScheme === 'light' ? LightTheme : DarkTheme
+
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    ;(async () => {
+      const isLoggedIn = await magic.user.isLoggedIn()
+      console.log('isLoggedIn', isLoggedIn)
+      setLoaded(true)
+    })()
+  }, [])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
 
   // TODO: use magic.Relayer conditionally
   // TODO: set local environment to be able to use local auth provider
