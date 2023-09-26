fecf27b1b5d6810bdeb2565459a251e83d0e646c
antklim <antklim@gmail.com>
2023-09-26 11:14:50 +1000

    chore: configuring jest test

--- .jest/jest.config.ts
+++ .jest/jest.config.ts
@@ -1,6 +1,14 @@
 export default {
+  moduleDirectories: ['node_modules'],
+  modulePath: ['<rootDir>'],
   preset: 'jest-expo',
+  rootDir: '..',
+  roots: ['<rootDir>'],
   setupFilesAfterEnv: ['<rootDir>/.jest/jestSetupAfterEnv.ts'],
+  transform: {
+    '\\.tsx?$': 'ts-jest',
+    '\\.jsx?$': 'babel-jest',
+  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
--- lib/auth/hooks.test.ts
+++ lib/auth/hooks.test.ts
@@ -1,5 +1,23 @@
+import { renderHook } from '@testing-library/react-native'
+import { useAuthInfo } from '.'
+
 describe('auth/useAuthInfo hook', () => {
-  test('TODO: add unit tests', () => {
-    expect(true).toBe(true)
+  test('returns state while loading', () => {
+    const auth = {
+      getInfo: () => Promise.resolve({ email: '' }),
+      isSignedIn: () => Promise.resolve(false),
+    }
+
+    const {
+      result: {
+        current: { loaded },
+      },
+    } = renderHook(() => useAuthInfo(auth))
+
+    expect(loaded).toBe(false)
   })
+
+  test.todo('returns signed in state when loaded')
+  test.todo('returns auth info when loaded and signed in')
+  test.todo('returns error when loading fails')
 })
