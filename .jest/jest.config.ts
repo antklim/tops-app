6a73edd9a324d5864e1b70c59764ca013bf4cd1d
antklim <antklim@gmail.com>
2023-09-26 13:49:57 +1000

    chore: updated deps

--- .jest/jest.config.ts
+++ .jest/jest.config.ts
@@ -5,10 +5,6 @@ export default {
   rootDir: '..',
   roots: ['<rootDir>'],
   setupFilesAfterEnv: ['<rootDir>/.jest/jestSetupAfterEnv.ts'],
-  transform: {
-    '\\.tsx?$': 'ts-jest',
-    '\\.jsx?$': 'babel-jest',
-  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
