794fd1323ed740c59a4af3517f55600603206d6f
antklim <antklim@gmail.com>
2023-09-28 15:43:05 +1000

    chore: tested auth hook

--- lib/auth/hooks.test.ts
+++ lib/auth/hooks.test.ts
@@ -1,17 +1,17 @@
-import { renderHook } from '@testing-library/react-native'
-import { useAuthInfo } from '.'
-import { delayedResolve } from 'test/delayedResult'
+import { renderHook, waitFor } from '@testing-library/react-native'
+import { AuthInfo, useAuthInfo } from '.'
+import { delayedReject, delayedResolve } from 'test/delayedResult'
 
 describe('auth/useAuthInfo hook', () => {
-  test('returns state while loading', () => {
+  test('returns initial state while loading', () => {
     const auth = {
-      getInfo: () => delayedResolve<{ email: string }>({ email: '' }),
+      getInfo: () => delayedResolve<AuthInfo>({ email: '' }),
       isSignedIn: () => delayedResolve(false, 200),
     }
 
     const {
       result: {
-        current: { loaded },
+        current: { loaded, signedIn, authInfo, error },
       },
       unmount,
     } = renderHook(() => useAuthInfo(auth))
@@ -19,9 +19,65 @@ describe('auth/useAuthInfo hook', () => {
     unmount()
 
     expect(loaded).toBe(false)
+    expect(signedIn).toBe(false)
+    expect(authInfo).toBe(undefined)
+    expect(error).toBe(undefined)
   })
 
-  test.todo('returns signed in state when loaded')
-  test.todo('returns auth info when loaded and signed in')
-  test.todo('returns error when loading fails')
+  test('returns signed in state when loaded', async () => {
+    const auth = {
+      getInfo: () => delayedResolve<AuthInfo>({ email: '' }),
+      isSignedIn: () => delayedResolve(false, 200),
+    }
+
+    const { result } = renderHook(() => useAuthInfo(auth))
+
+    expect(result.current.loaded).toBe(false)
+
+    await waitFor(() => {
+      expect(result.current.loaded).toBe(true)
+    })
+
+    expect(result.current.signedIn).toBe(false)
+    expect(result.current.authInfo).toBe(undefined)
+    expect(result.current.error).toBe(undefined)
+  })
+
+  test('returns auth info when loaded and signed in', async () => {
+    const auth = {
+      getInfo: () => delayedResolve<AuthInfo>({ email: 'hello@tops.app' }),
+      isSignedIn: () => delayedResolve(true, 200),
+    }
+
+    const { result } = renderHook(() => useAuthInfo(auth))
+
+    expect(result.current.loaded).toBe(false)
+
+    await waitFor(() => {
+      expect(result.current.loaded).toBe(true)
+    })
+
+    expect(result.current.signedIn).toBe(true)
+    expect(result.current.authInfo).toEqual({ email: 'hello@tops.app' })
+    expect(result.current.error).toBe(undefined)
+  })
+
+  test('returns error when loading fails', async () => {
+    const auth = {
+      getInfo: () => delayedReject<AuthInfo>(new Error('Failed to get info')),
+      isSignedIn: () => delayedResolve(true, 200),
+    }
+
+    const { result } = renderHook(() => useAuthInfo(auth))
+
+    expect(result.current.loaded).toBe(false)
+
+    await waitFor(() => {
+      expect(result.current.loaded).toBe(true)
+    })
+
+    expect(result.current.signedIn).toBe(true)
+    expect(result.current.authInfo).toBe(undefined)
+    expect(result.current.error).toEqual(new Error('Failed to get info'))
+  })
 })
--- test/delayedResult.ts
+++ test/delayedResult.ts
@@ -1,5 +1,5 @@
 export const delayedResolve = <T>(v: T, ms?: number): Promise<T> =>
   new Promise((resolve) => setTimeout(() => resolve(v), ms ?? 100))
 
-export const delayedReject = (e: Error, ms?: number): Promise<Error> =>
+export const delayedReject = <T>(e: Error, ms?: number): Promise<T> =>
   new Promise((_, reject) => setTimeout(() => reject(e), ms ?? 100))
