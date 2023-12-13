7cc3422b13eded9ba4457eb3eb3b373748da14a1
antklim <antklim@gmail.com>
2023-12-13 14:46:21 +1100

    refactor: created features folder

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'react-native'
+import { Text, YStack } from 'tamagui'
+
+export const Home = () => {
+  const colorScheme = useColorScheme()
+
+  return (
+    <YStack
+      theme="green"
+      fullscreen
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      padding="$3"
+      minWidth={300}
+      space="$4">
+      <Text>This is home page</Text>
+      <Text>Scheme name {colorScheme}</Text>
+      <StatusBar style="auto" />
+    </YStack>
+  )
+}
---
+++ src/features/sign-in/SignIn.tsx
@@ -0,0 +1,49 @@
+import { ActivityIndicator } from 'react-native'
+import { useState } from 'react'
+import { useAuth } from 'src/context/auth'
+import { Button, Input, Spinner, XStack, YStack } from 'tamagui'
+
+export const SignIn = () => {
+  const { signIn } = useAuth()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [signingIn, setSigningIn] = useState(false)
+
+  const submitSignIn = async () => {
+    setSigningIn(true)
+
+    try {
+      await signIn({ name, email })
+    } catch {
+    } finally {
+      setSigningIn(false)
+    }
+  }
+
+  return (
+    <YStack
+      theme="green"
+      fullscreen
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      padding="$3"
+      minWidth={300}
+      space="$4">
+      <XStack alignItems="center" space="$4">
+        <Input flex={1} id="name" placeholder="Name" onChangeText={setName} />
+      </XStack>
+
+      <XStack alignItems="center" space="$4">
+        <Input flex={1} id="email" placeholder="Email" onChangeText={setEmail} />
+      </XStack>
+
+      {signingIn && <ActivityIndicator />}
+
+      <Button icon={signingIn ? () => <Spinner /> : undefined} onPress={submitSignIn}>
+        Sign In
+      </Button>
+    </YStack>
+  )
+}
