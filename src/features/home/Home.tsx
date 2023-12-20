afd5376587f5281bc143350d03ca291344cb3c8f
antklim <antklim@gmail.com>
2023-12-20 16:30:13 +1100

    refactor: gym card moved to a separate component

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,48 +1,6 @@
 import { FlatList } from 'react-native'
-import { GymLogo } from 'src/ui/components/GymLogo'
-import { Button, Card, CardProps, H2, Heading, Paragraph, Spacer, XStack, YStack } from 'tamagui'
-
-interface GymData {
-  gymName: string
-  weekNumber: number
-  weekEndsOn: Date
-  nextWeekStartsOn: Date
-}
-
-type GymCardProps = CardProps & GymData
-
-const GymCard = ({ gymName, weekNumber, weekEndsOn, nextWeekStartsOn, ...props }: GymCardProps) => {
-  const weekEnds = weekEndsOn.toLocaleDateString('en-GB', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  })
-
-  const nextWeekStart = nextWeekStartsOn.toLocaleDateString('en-GB', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  })
-
-  return (
-    <Card size="$4" bordered {...props}>
-      <Card.Header padded>
-        <H2>{gymName}</H2>
-        <Paragraph theme="alt2">
-          Week {weekNumber} (until {weekEnds})
-        </Paragraph>
-        <Paragraph theme="alt1_Progress">New climbs on {nextWeekStart}</Paragraph>
-      </Card.Header>
-      <Card.Footer padded>
-        <XStack flex={1} />
-        <Button borderRadius="$10">Go to gym</Button>
-      </Card.Footer>
-      <Card.Background>
-        <GymLogo />
-      </Card.Background>
-    </Card>
-  )
-}
+import { Heading, Spacer, YStack } from 'tamagui'
+import { GymCard, GymData } from './GymCard'
 
 export const Home = () => {
   const gymData: GymData[] = [
