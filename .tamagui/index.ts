bc034f343f5b6b1771f1b9a6dfe7b9fdb67e3dc9
antklim <antklim@gmail.com>
2024-01-11 14:45:50 +1100

    chore: added tamagui shorthands

--- .tamagui/index.ts
+++ .tamagui/index.ts
@@ -1,11 +1,13 @@
 import { config } from '@tamagui/config/v2-native'
 import { createTamagui } from 'tamagui'
 import { themes, tokens } from '@tamagui/themes'
+import { shorthands } from '@tamagui/shorthands'
 import fonts from './fonts'
 
 const appConfig = createTamagui({
   ...config,
   fonts,
+  shorthands,
   themes,
   tokens,
 })
